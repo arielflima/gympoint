9680fe1baef2725f6ea98549e162b29f916c8abc
arielflima <arielflima@gmail.com>
2019-10-17 12:01:03 -0300

    Configurando eslint/prettier/editorconfig

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ class App {
 
     this.middlewares();
     this.routes();
-  };
+  }
 
   middlewares() {
     this.server.use(express.json());
-  };
+  }
 
   routes() {
     this.server.use(routes);
-  };
-};
+  }
+}
 
 export default new App().server;
